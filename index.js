15dbc200e0293e618a7d029a90be46ec69d8886f
ionnotion <adryan.rahadianto@gmail.com>
2022-09-15 23:09:17 +0700

    resolve validasi login & update user data

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-//RNG untuk session ID
-function generateSessionId() {
+function generateSessionId() { //RNG untuk session ID
     let strNum = ''
     let output = ''
     
@@ -15,99 +14,33 @@ function getRndInteger(min, max) { //random untuk profile picture user
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-let profilePictureArr = ["hackaton8/profile_picture_comp/1.jpg","hackaton8/profile_picture_comp/2.jpg","hackaton8/profile_picture_comp/3.jpg","hackaton8/profile_picture_comp/4.jpg","hackaton8/profile_picture_comp/5.jpg"] //url profile picture user
-                // 3 data user, classnya: 
-let sessionId = generateSessionId()                                                 // session-id
-let displayName                                                                     // display-name
-let profilePicture = profilePictureArr[getRndInteger(0,profilePictureArr.length-1)] // profile-picture
+let profilePictureArr = ["./profile_picture_comp/1.jpg","./profile_picture_comp/2.jpg","./profile_picture_comp/3.jpg","./profile_picture_comp/4.jpg","./profile_picture_comp/5.jpg"] //url profile picture user
 
-let userSessionIdElement = document.getElementById('session-id')
-let userDisplayNameElement = document.getElementById('display-name')
-let userProfilePictureElement = document.getElementById('profile-picture')
-
+function updateUserData() {
+    let sessionId = generateSessionId()                                                 // session-id
+    let displayName = sessionStorage.getItem("displayName")                             // display-name
+    let profilePicture = profilePictureArr[getRndInteger(0,profilePictureArr.length-1)] // profile-picture
     
-    window.onchange = function() {
-        userDisplayNameElement.innerText = displayName
-        userSessionIdElement.innerText  = sessionId
-        userProfilePictureElement.src   = profilePicture
-    }
-    // console.log(displayName)
-    // console.log(sessionId)
-    // console.log(profilePicture)
+    let userSessionIdElement = document.getElementById('session-id')
+    let userDisplayNameElement = document.getElementById('display-name')
+    let userProfilePictureElement = document.getElementById('profile-picture')
 
-    console.log(userSessionIdElement.innerText)
-    console.log(userDisplayNameElement.innerText)
-    console.log(userProfilePictureElement.src)
+    userSessionIdElement.innerText = sessionId
+    userDisplayNameElement.innerText = displayName
+    userProfilePictureElement.src = profilePicture
 
+    sessionStorage.removeItem("displayName");
+}
+
+updateUserData()
 
 let feedBox = document.getElementById("feed-box")
 let topicSelectorRadio = document.getElementById("topic-selector") // radio untuk memilih topic TWOT
 let selectTopicCheckbox = document.getElementById("topic-checkbox") // checkbox untuk menampilkan TWOT yang relevan
 
-let displayNameInputElement = document.getElementById("display-name-input")
-
-const submitDisplayNameButton = document.getElementById("signIn")
-
 const submitFeedButton = document.getElementById("submit-twot") // elemen button untuk submit feed
-
 const deleteTwotButton = document.getElementById("delete-twot")
-
 let twotCounter = 0
-
-submitDisplayNameButton.addEventListener('click', function () {
-    
-    let displayNameCheck = displayNameInputElement.value
-    if (displayNameCheck === '') {
-        return alert('Silahkan masukkan nama yang ingin ditampilkan')
-    }
-    
-    if (displayNameCheck.length < 4 && displayNameCheck.length > 15) {
-        return alert('Masukkan display name dengan 4-15 alphabet')
-    }
-
-    if (!(/^[a-zA-Z]+$/.test(displayNameCheck))) {
-        return alert('Display name tidak boleh berisi angka atau symbol')
-    }
-
-    // location.href = "index.html"  // pindah ke page selanjutnya?
-    // console.log(displayNameCheck)
-        userDisplayNameElement.innerText = displayNameCheck
-        userSessionIdElement.innerText = sessionId
-        userProfilePictureElement.src = profilePicture
-    // console.log(sessionId)
-})
-
-displayNameInputElement.addEventListener('input', function () {
-
-    let actionName = document.getElementById('loginprocess')
-    console.log(actionName)
-    
-    let displayNameCheck = displayNameInputElement.value
-    let isNotEmpty = false
-    let isMoreThen4 = false
-    let isAlphabet = false
-    
-    if (displayNameCheck !== '') {
-        isNotEmpty = true
-    }
-    
-    if (displayNameCheck.length > 4 && displayNameCheck.length < 15) {
-        isMoreThen4 = true
-    }
-
-    if ((/^[a-zA-Z]+$/.test(displayNameCheck))) {
-        isAlphabet = true
-    }
-
-    console.log(isNotEmpty,isMoreThen4,isAlphabet)
-
-    if(isNotEmpty && isMoreThen4 && isAlphabet) {
-        actionName.action = 'index.html'
-    } else {
-        actionName.action = ""
-    }
-
-})
 
 submitFeedButton.addEventListener('click', function () {
     if (!feedBox.value) {
