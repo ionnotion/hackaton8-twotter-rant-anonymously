1d79c3084c711eee703355fcb1043afd12fee316
ionnotion <adryan.rahadianto@gmail.com>
2022-09-15 16:30:24 +0700

    submit display name

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 //RNG untuk session ID
-
 function generateSessionId() {
     let strNum = ''
     let output = ''
     
-        while (strNum.length < 9){
-            let number = Math.floor(Math.random()*10)
-            strNum+= number.toString();
-        }
-        output = `#${strNum}`
-        console.log(output);
+    while (strNum.length < 9){
+        let number = Math.floor(Math.random()*10)
+        strNum+= number.toString();
+    }
+    output = `#${strNum}`
 }
-const submitFeedButton = document.getElementById('submit-feed') // elemen button untuk submit feed
 
 let sessionId = generateSessionId()
-let displayName = document.getElementById('displayname').value
-    
+let displayName
 let feedBox = document.getElementById('feed-box')
-let topicSelector = document.getElementById('topic-selector')
+let topicSelector = document.getElementById('topic-selector') // radio untuk memilih topic TWOT
+let twotCounter = 0
+const submitDisplayNameButton = document.getElementById('submit-display-name')
+const submitFeedButton = document.getElementById('submit-feed') // elemen button untuk submit feed
+// const deleteTwotButton = document.q
 
 submitFeedButton.addEventListener('click', function () {
 
-    let answer = '';
-    
-    if (feedBox.value) {
+    if (!feedBox.value) {
         return alert('Silahkan masukkan keluhanmu')
     }
     
     let feedList = document.getElementById('feed-list') // elemen yang akan menampilkan feed-feed
     let divTwot = document.createElement('div') // how to add class to an element
-    /* rangkai elemen twot (session ID, DisplayName, ProfilePicture, Twot)
+    divTwot.classList.add(`Twot${twotCounter}`)
+    twotCounter++
+    /* rangkai elemen twot (session ID, DisplayName, ProfilePicture, TWOT)
     
     
     */
@@ -37,3 +37,22 @@ submitFeedButton.addEventListener('click', function () {
     alert(`Jawaban ${answer}`)
 })
 
+submitDisplayNameButton.addEventListener('click', function () {
+    
+    let displayNameCheck = submitDisplayNameButton.value
+    if (displayNameCheck === '') {
+        return alert('Silahkan masukkan nama yang ingin ditampilkan')
+    }
+    
+    if (displayNameCheck.length < 4 && displayNameCheck.length > 15) {
+        return alert('Masukkan display name dengan 4-15 alphabet')
+    }
+
+    var hasNumber = /\d/;   
+      hasNumber.test(displayNameCheck)
+    
+    if (hasNumber) return alert(`Display name tidak boleh mengandung angka dan symbol`)
+    displayName = displayNameCheck      // kalau dipush assign value ke display name
+                                        // 
+    window.location.href = "main.html"  // pindah ke page selanjutnya?
+})
