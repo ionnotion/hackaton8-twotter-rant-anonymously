76e81952d0c4443eff336d8da0843f270c3901e4
ivanarifin <ivan.rarifin@gmail.com>
2022-09-16 10:07:50 +0700

    penambahan filter output yang sama

--- feed.js
+++ feed.js
@@ -8,49 +8,65 @@ document.getElementById("tweet3").appendChild(clone2);
 
 let profilePictureArr = ["./profile_picture_comp/1.jpg","./profile_picture_comp/2.jpg","./profile_picture_comp/3.jpg","./profile_picture_comp/4.jpg","./profile_picture_comp/5.jpg"]
 let tweetsArr = ["Saya tidak suka bermain paaji mobile", "Kenapa tim merah kalah terus!!!", "Film kapten tisubasa tidak masuk akal", "Game PA ini benerbener pay2win","Harga game dari EB selalu mahal", "Ini tim gak jelas pelatihnya", "Gamenya ada bug terus kayak masih beta, developernya gak niat ini", "lagunya bikin kuping sakit", "filmnya kelamaan, alurnya juga gak bagus","Masa iya tim profesional stadionnya kayak kandang", 'Nama doang ada jordannya, padahal gak bisa main basket']
-let usersArr = ["Makrusus", "Pesa", "Fethir", "Onad", "kiki", "jenud", "pakjul", "akhirudin", "wali", "andrea", "tidar"]
+let usersArr = ["Makrusus", "Pesa", "Fethir", "Onad", "kiki", "jenud", "pakjul", "akhirudin", "wali", "andreak", "tidar"]
+let tempArr =[]
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-function shuffle(array) {
-    var i = array.length,
-        j = 0,
-        temp;
+// function shuffle(array) {
+//     var i = array.length,
+//         j = 0,
+//         temp;
 
-    while (i--) {
+//     while (i--) {
 
-        j = Math.floor(Math.random() * (i+1));
+//         j = Math.floor(Math.random() * (i+1));
 
-        // swap randomly chosen element with current element
-        temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+//         // swap randomly chosen element with current element
+//         temp = array[i];
+//         array[i] = array[j];
+//         array[j] = temp;
 
-    }
-    return array[0]
+//     }
+//     return array[0]
 
-}
+// }
 
 
 let image = document.getElementsByClassName("foto")
 for (let i = 0; i < image.length; i++) {
     let profilePicture = profilePictureArr[getRndInteger(0,profilePictureArr.length-1)]
-    image[i].src = profilePicture
+    if (!(tempArr.includes(profilePicture))) {
+        image[i].src = profilePicture
+        tempArr.push(profilePicture)
+    } else{
+        i--
+    }
 }
 
 let twit = document.getElementsByClassName("twit")
 for (let i = 0; i < twit.length; i++) {
-    // let twits = tweetsArr[getRndInteger(0,tweetsArr.length-1)]
-    let twits = shuffle(tweetsArr)
-    twit[i].innerText = twits
+    // let twits = shuffle(tweetsArr)
+    let twits = tweetsArr[getRndInteger(0,tweetsArr.length-1)]
+    if (!(tempArr.includes(twits))) {
+        twit[i].innerText = twits
+        tempArr.push(twits)
+    } else{
+        i--
+    }
 }
 
 let nama = document.getElementsByClassName("nama")
 for (let i = 0; i < nama.length; i++) {
     let namas = usersArr[getRndInteger(0,usersArr.length-1)]
-    nama[i].innerText = namas
+    if (!(tempArr.includes(namas))) {
+        nama[i].innerText = namas
+        tempArr.push(namas)
+    } else{
+        i--
+    }
 }
 
 let sessionId = document.getElementsByClassName("RNG")
